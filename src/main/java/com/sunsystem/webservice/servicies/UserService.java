304ebcd805598ebcbc7f973fc2bfa67f749b47ce
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-12-28 20:51:29 -0400

    Exception handling - findById

--- src/main/java/com/sunsystem/webservice/servicies/UserService.java
+++ src/main/java/com/sunsystem/webservice/servicies/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.sunsystem.webservice.entity.User;
 import com.sunsystem.webservice.repository.UserRepository;
+import com.sunsystem.webservice.servicies.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
